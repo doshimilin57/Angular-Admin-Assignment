76a9c99251546f9aec84152d3831995b819fe963
Milin Doshi <doshimilin57@gmail.com>
2020-06-16 14:23:06 -0400

    Angular- Admin - Panel Project check-in!

---
+++ src/app/utitlity.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtitlityService {
+  constructor(public router: Router, public activeRoute: ActivatedRoute) {}
+
+  goBack() {
+    window.history.back();
+  }
+
+  getUsers() {
+    return JSON.parse(localStorage.getItem('users'));
+  }
+
+  saveUser(data) {
+    localStorage.setItem('users', data);
+  }
+
+  getDevices() {
+    return JSON.parse(localStorage.getItem('devices'));
+  }
+
+  saveDevice(data) {
+    localStorage.setItem('devices', data);
+  }
+  navigate(url) {
+    this.router.navigateByUrl(url);
+  }
+  generateId() {
+    return (Math.random() * 1000000).toFixed(0);
+  }
+
+  getQueryParam() {
+    return this.activeRoute.queryParams;
+  }
+}
